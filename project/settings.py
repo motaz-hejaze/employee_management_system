90542c02a1f8e6a1eb324db308152a4a090716e9
motaz-hejaze <trapperx.1@gmail.com>
2020-12-16 23:40:28 +0200

    static

--- project/settings.py
+++ project/settings.py
@@ -131,7 +131,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_main')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
